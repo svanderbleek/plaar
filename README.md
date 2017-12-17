607dce421dfc35a03c84f9ceeb0fddfb74c1132e
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2017-12-16 20:40:36 -0500

    start chapter1

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Practical Logic and Automated Reasoning
 
-Papers and Exercises
+Papers and Exercises and Projects.
+
+# Projects
+
+- "In everyday mathematics we usually rely on informal cues like variable names and background knowledge
+to see at once that `f(x+1)` denotes function application, whereas `y(x+1)` denotes multiplication, but this kind of context-dependent parsing is a bit more complicated to implement."
--- chapter1.ml
+++ chapter1.ml
@@ -5,3 +5,25 @@ type expression
   | Mul of expression * expression
 
 let ex1 = Add(Mul(Const 2, Var "x"), Var "")
+
+let simplify1 expr =
+  match expr with
+    Add(Const(x), Const(y)) -> Const(x + y)
+  | Add(Const(0), x) -> x
+  | Add(x, Const(0)) -> x
+  | Mul(Const(x), Const(y)) -> Const(x * y)
+  | Mul(x, Const(0)) -> Const(0)
+  | Mul(Const(0), x) -> Const(0)
+  | Mul(Const(1), x) -> x
+  | Mul(x, Const(1)) -> x
+  | _ -> expr
+
+let rec simplify expr =
+  match expr with
+    Add(x, y) -> simplify1(Add(simplify x, simplify y))
+  | Mul(x, y) -> simplify1(Mul(simplify x, simplify y))
+  | _ -> simplify1 expr
+
+let ex2 = Add(Mul(Add(Mul(Const(0), Var("x")), Const(1)), Const(3)), Const(12))
+
+let re2 = simplify(ex2)
