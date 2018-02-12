cd07f6efad2bbe6cb592d9b550c83bb2305e1602
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2018-02-11 22:27:59 -0500

    in the middles

--- chapter2.ml
+++ chapter2.ml
@@ -58,66 +58,6 @@ let parse_simpl =
   let dummy = fun a _ -> (True, a) in
   make_parser (parse_formula (dummy, dummy) [])
 
-let psd =
-  make_parser
-    (parse_formula ((fun _ _ -> failwith ""), parse_propvar) [])
-
-let mk_and p q = And(p, q) and mk_or p q = Or(p, q)
-and mk_imp p q = Imp(p, q) and mk_iff p q = Iff(p, q)
-and mk_forall x p = Forall(x, p) and mk_exists x p = Exists(x, p)
-
-let _iff fm =
-  match fm with
-    Iff(p, q) -> (p, q)
-  | _ -> failwith "_iff"
-
-let _imp fm =
-  match fm with
-    Imp(p, q) -> (p, q)
-  | _ -> failwith "_imp"
-
-let _and fm =
-  match fm with
-    And(p, q) -> (p, q)
-  | _ -> failwith "_and"
-
-let rec conjuncts fm =
-  match fm with
-    And(p, q) -> conjuncts p @ conjuncts q
-  | _ -> [fm]
-
-let rec _or fm =
-  match fm with
-    Or(p, q) -> (p, q)
-  | _ -> failwith "_or"
-
-let rec disjuncts fm =
-  match fm with
-    Or(p, q) -> disjuncts p @ disjuncts q
-  | _ -> [fm]
-
-let antecedent fm = fst(_imp fm)
-let consequent fm = snd(_imp fm)
-
-let rec onatoms f fm =
-  match fm with
-    Atom a -> f a
-  | Not(p) -> Not(onatoms f p)
-  | And(p, q) -> And(onatoms f p, onatoms f q)
-  | Or(p, q) -> Or(onatoms f p, onatoms f q)
-  | Imp(p, q) -> Imp(onatoms f p, onatoms f q)
-  | Iff(p, q) -> Iff(onatoms f p, onatoms f q)
-  | Forall(x, p) -> Forall(x, onatoms f p)
-  | Exists(x, p) -> Exists(x, onatoms f p)
-  | _ -> fm
-
-let rec overatoms f fm b =
-  match fm with
-    Atom(a) -> f a b
-  | And(p, q) | Or(p, q) | Imp(p, q) | Iff(p, q) -> overatoms f p (overatoms f q b)
-  | Forall(x, p)  | Exists(x, p) -> overatoms f p b
-  | _ -> b
-
 let rec strip_quant fm =
   match fm with
       Forall(x, (Forall(y, p) as yp)) | Exists(x, (Exists(y, p) as yp)) ->
@@ -154,5 +94,105 @@ let print_formula pfn =
 
 let print_propvar prec p = print_string(pname p)
 
-let prd = print_formula print_propvar
+let psf =
+  make_parser
+    (parse_formula ((fun _ _ -> failwith ""), parse_propvar) [])
 
+let prf = print_formula print_propvar
+
+let mk_and p q = And(p, q) and mk_or p q = Or(p, q)
+and mk_imp p q = Imp(p, q) and mk_iff p q = Iff(p, q)
+and mk_forall x p = Forall(x, p) and mk_exists x p = Exists(x, p)
+
+let dest_iff fm =
+  match fm with
+    Iff(p, q) -> (p, q)
+  | _ -> failwith "_iff"
+
+let dest_imp fm =
+  match fm with
+    Imp(p, q) -> (p, q)
+  | _ -> failwith "_imp"
+
+let dest_and fm =
+  match fm with
+    And(p, q) -> (p, q)
+  | _ -> failwith "_and"
+
+let rec conjuncts fm =
+  match fm with
+    And(p, q) -> conjuncts p @ conjuncts q
+  | _ -> [fm]
+
+let rec dest_or fm =
+  match fm with
+    Or(p, q) -> (p, q)
+  | _ -> failwith "_or"
+
+let rec disjuncts fm =
+  match fm with
+    Or(p, q) -> disjuncts p @ disjuncts q
+  | _ -> [fm]
+
+let antecedent fm = fst(dest_imp fm)
+let consequent fm = snd(dest_imp fm)
+
+let rec onatoms f fm =
+  match fm with
+    Atom a -> f a
+  | Not(p) -> Not(onatoms f p)
+  | And(p, q) -> And(onatoms f p, onatoms f q)
+  | Or(p, q) -> Or(onatoms f p, onatoms f q)
+  | Imp(p, q) -> Imp(onatoms f p, onatoms f q)
+  | Iff(p, q) -> Iff(onatoms f p, onatoms f q)
+  | Forall(x, p) -> Forall(x, onatoms f p)
+  | Exists(x, p) -> Exists(x, onatoms f p)
+  | _ -> fm
+
+let rec overatoms f fm b =
+  match fm with
+    Atom(a) -> f a b
+  | And(p, q) | Or(p, q) | Imp(p, q) | Iff(p, q) -> overatoms f p (overatoms f q b)
+  | Forall(x, p)  | Exists(x, p) -> overatoms f p b
+  | _ -> b
+
+let atom_union f fm = List.sort_uniq (fun a b -> -1) (overatoms (fun h t -> f(h)@t) fm [])
+
+let atoms fm = atom_union (fun a -> [a]) fm
+
+let rec eval fm v =
+  match fm with
+    False -> false
+  | True -> true
+  | Atom(x) -> v(x)
+  | Not(p) -> not(eval p v)
+  | And(p, q) -> (eval p v) && (eval q v)
+  | Or(p, q) -> (eval p v) || (eval q v)
+  | Imp(p, q) -> not(eval p v) or (eval q v)
+  | Iff(p, q) -> (eval p v) = (eval q v)
+  | _ -> false
+
+let rec onallvaluations subfn v ats =
+  match ats with
+    [] -> subfn v
+  | p::ps ->
+    let v' t q = if q = p then t else v(q) in
+    onallvaluations subfn (v' false) ps &&
+    onallvaluations subfn (v' true) ps
+
+let ( ** ) =
+
+let print_truthtable fm =
+  let ats = atoms fm in
+  let width = List.iter (fun s -> max ** String.length ** pname) ats in
+  let fixw s = s ^ String.make(width - String.length s) ' ' in
+  let truthstring p = fixw (if p then "true" else "false") in
+  let mk_row v =
+    let lis = map (fun x -> truthstring(v x)) ats
+    and ans = truthstring(eval fm v) in
+    print_string(List.iter (^) list ("| " ^ ans)); print_newline(); true in
+  let separator = String.make (width * length ats + 9) '-' in
+  print_string (List.iter (fun s t -> fixw(pname s) ^ t) ats "| formula");
+  print_newline(); print_string separator; print_newline();
+  let _ = onallvaluations mk_row (fun x -> false) ats in
+  print_string separator; print_newline()
